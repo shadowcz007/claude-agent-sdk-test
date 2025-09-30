58db7bc5e6c243e6a3484d82f3fea5825db77c9e
shadow <chizhiwei007@163.com>
2025-09-30 11:14:54 +0800

    Update test.js

--- test.js
+++ test.js
@@ -39,3 +39,24 @@ async function runWithCustomSystemPrompt() {
         }
     }
 }
+
+
+
+async function runWithCustomSystemPromptAndCwd() {
+    const targetDir = '/Users/shadow/Documents/GitHub/claude-agent-sdk-test/test';
+    const stream = query({
+        prompt: "帮我创建一个md文档，关于PPT Agent的介绍",
+        options: {
+            cwd: targetDir,
+            systemPrompt: "你是一个专业的产品设计师，擅长调研用户需求，设计独特的产品功能，富有乔布斯演讲的叙事能力。根据用户的指令创建ppt，不需要询问用户，直接创建",
+            model: 'google/gemini-2.5-flash',
+            permissionMode: 'bypassPermissions' // 简化权限控制
+        }
+    });
+
+    for await (const message of stream) {
+        if (message.type === 'assistant') {
+            console.log(message.message.content[0].text);
+        }
+    }
+}
